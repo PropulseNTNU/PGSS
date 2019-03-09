c299edb846166ec8ee385a94d984c8cd9e16d075
Even <eaeliass@stud.ntnu.no>
2019-03-09 11:36:15 +0100

    Removed flag functionality

--- _src/xbee.cpp
+++ _src/xbee.cpp
@@ -1,45 +1,12 @@
-#include "gs.h"
-
-void byte_to_bools(uint8_t bools[], const uint8_t number_of_bools, const uint8_t *byte_with_bools) {
-	for (int i = 0; i < number_of_bools; ++i) {
-		bools[i] = *byte_with_bools & (1 << i);
-	}
-}
-
-
-void read_flags(uint8_t *data, uint8_t count, uint16_t *package_number, int size_compressed_flags, uint8_t *flags, int flags_size) {
-	uint8_t number_of_bools;
-
-	//Calculate number of bools in compressed byte. It is 8 unless receiving the last byte of flags.
-	if (count < sizeof(*package_number) - 1 + size_compressed_flags) {
-		number_of_bools = 8;
-	}
-	else {
-		number_of_bools = flags_size % 8;
-	}
-	//Decompress
-	byte_to_bools(&flags[(count - sizeof(*package_number)) * 8], number_of_bools, data);
-}
-
-
+#include "xbee.h"
 
 uint8_t read_buffer(uint8_t *buffer,   int buffer_size, 
 			 uint8_t *sensors,  int sensors_size,
-			 uint8_t *flags,	int flags_size,
 			 uint16_t *package_number) {
-
-	//Calculating size of compressed flags
-	uint8_t size_compressed_flags;
-	if (flags_size == 0) {
-		size_compressed_flags = 0;
-	}
-	else {
-		size_compressed_flags = (flags_size - 1) / 8 + 1;
-	}
 
 	
 	uint8_t data;
-	int package_size = sizeof(*package_number) + size_compressed_flags + sensors_size; 
+	int package_size = sizeof(*package_number) + sensors_size; 
 
 	//Find end byte in buffer
 	int i = buffer_size;
@@ -57,13 +24,9 @@ uint8_t read_buffer(uint8_t *buffer,   int buffer_size,
 		if (j < sizeof(*package_number)) {
 			((uint8_t*) package_number)[j] = data;
 		}
-		else if (j < sizeof(*package_number) + size_compressed_flags) {
-			read_flags(&data, j, package_number, size_compressed_flags, flags, flags_size);
-		}
 		else if (j < package_size) {
-			sensors[j - sizeof(*package_number) - size_compressed_flags] = data;
+			sensors[j - sizeof(*package_number)] = data;
 		}
-
 	}
 	return i-1;
 }
--- _src/xbee.h
+++ _src/xbee.h
@@ -1,15 +1,27 @@
-#ifndef GS_H
-#define GS_H
+#ifndef XBEE_H
+#define XBEE_H
 #include <stdint.h>
+
+enum sensorData { TIMESTAMP, BME_TEMP, IMU_TEMP, 
+                  PRESSURE, ALTITUDE, 
+                  ACC_X, ACC_Y, ACC_Z, 
+                  ROLL, PITCH, YAW, 
+                  MAG_X, MAG_Y, MAG_Z,
+                  ANGULAR_VEL_X,ANGULAR_VEL_Y,ANGULAR_VEL_Z,
+                  GRAVITY_ACC_X,GRAVITY_ACC_Y,GRAVITY_ACC_Z, 
+                  LINEAR_ACCEL_X,LINEAR_ACCEL_Y,LINEAR_ACCEL_Z,
+                  QUATERNION_X, QUATERNION_Y, QUATERNION_Z, QUATERNION_W,
+                  STATE, NUM_TYPES
+              };
+
+
 
 /*
  * Read last data package from buffer to flags[], sensors[] and package_number.
  * Return index of package start byte '<' in buffer
  */
-
-uint8_t read_buffer( uint8_t  *buffer,			int buffer_size,
-					 uint8_t  *sensors,			int sensors_size,
-					 uint8_t  *flags,			int flags_size,
+uint8_t read_buffer( uint8_t  *buffer, int buffer_size, 
+					 uint8_t  *sensors, int sensors_size,
 					 uint16_t *package_number);
 
 
